ea56c862d2642d119aed6545b731ae174a2236f4
José Henrique <josehenriquehyttspf@gmail.com>
2022-07-21 00:16:35 -0300

    add loadLocalTodo module

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,5 @@
 import setDate from "./modules/setDate.js";
+import loadLocalTodo from './modules/loadLocalTodo.js';
 
 setDate();
+loadLocalTodo();
---
+++ assets/scripts/modules/loadLocalTodo.js
@@ -0,0 +1,24 @@
+export default function loadLocalTodo() {
+  if (window.localStorage) {
+    const localData = window.localStorage;
+    const todo_list = document.getElementById('todo-list')
+    for (let i = 0; i <= localData.length; i++) {
+      if (localData[`todo_${i}`]) {
+        const data = JSON.parse(localData.getItem(`todo_${i}`))
+
+        const li = document.createElement('li'),
+          title = document.createElement('span'),
+          icon = document.createElement('i');
+
+        li.setAttribute('data-todo-status', data.todo_status);
+        li.setAttribute('data-todo-id', data.todo_id);
+        title.textContent = data.todo_title;
+
+        li.appendChild(title);
+        li.appendChild(icon);
+
+        todo_list.appendChild(li)
+      }
+    }
+  }
+}
